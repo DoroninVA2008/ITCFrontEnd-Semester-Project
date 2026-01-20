03da471e2ccbe2c36c227afb8eab0a5f27ef48f7
DoroninVA2008 <hotlive95334@gmail.com>
2026-01-20 18:27:11 +0500

    init Creacte AppSite 1

---
+++ ITCFrontEnd-CourSemester-Project/Vite+React+TypeScript/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>Hello, <a href="../public/index.html">Vite+React+TypeScript</a>!</h1>
+  </React.StrictMode>,
+)
