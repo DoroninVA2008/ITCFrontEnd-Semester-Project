6ef3d3cb909bfcfe14a693deed4222abb0391613
DoroninVA2008 <hotlive95334@gmail.com>
2026-04-03 23:14:27 +0500

    ComPush before starting connecting Panel Requests from BackEnd API to FrontEnd React 108

--- ITCFrontEnd-CourSemester-Project/Vite+React+TypeScript/src/app/app.tsx
+++ ITCFrontEnd-CourSemester-Project/Vite+React+TypeScript/src/app/app.tsx
@@ -9,6 +9,7 @@ import { Mapp } from '../pages/map/mapp.tsx'
 import { LogIn } from '../pages/login/login.tsx'
 import { Admin } from '../pages/admin/admin.tsx'
 import { HisLog } from '../pages/hislog/hislog.tsx'
+import { Moder } from '../pages/moder/moder.tsx'
 
 const rootElement = document.getElementById('root');
   if (rootElement) {
@@ -23,6 +24,7 @@ const rootElement = document.getElementById('root');
               <Route path="/log" element={<LogIn />} />
               <Route path="/adm" element={<Admin />} />
               <Route path="/his" element={<HisLog />} />
+              <Route path="/mod" element={<Moder />} />
             </Routes>
           </BrowserRouter>
         </Provider>
--- ITCFrontEnd-CourSemester-Project/Vite+React+TypeScript/src/entities/cons.ts
+++ ITCFrontEnd-CourSemester-Project/Vite+React+TypeScript/src/entities/cons.ts
@@ -5,4 +5,11 @@ export var formApi = 'https://api.russia-heroes.ru/api/requests'; // ./src/entit
 export var eventsListTypes = 'https://api.russia-heroes.ru/api/event-types'; // ./src/entities/event-types.json
 export var card = (id: number) => `https://api.russia-heroes.ru/api/objects/${id}`; // ./src/entities/object${id}.json
 export const cards: string[] = Array.from({ length: 23 }, (_, i) => card(i + 1)); // https://api.russia-heroes.ru/docs
-export var admin = 'https://api.russia-heroes.ru/api/admin/requests'
+export var adminLogIn = 'https://api.russia-heroes.ru/api/admin/login';
+export var adminReFresh = 'https://api.russia-heroes.ru/api/admin/refresh';
+export var adminLogOut = 'https://api.russia-heroes.ru/api/admin/logout';
+export var adminList = 'https://api.russia-heroes.ru/api/admin/requests'; // ./src/entities/admin-requests.json
+export var adminCard = (id: number) => `https://api.russia-heroes.ru/api/admin/requests/${id}`; // ./src/entities/admin-requests.json
+export const adminCards: string[] = Array.from({ length: 23 }, (_, i) => adminCard(i + 1));
+export var admins = 'https://api.russia-heroes.ru/api/admin/admins';
+export var DelAdmins = (id: number) => `https://api.russia-heroes.ru/api/admin/admins/${id}`;
